79544b1d6dcabec50b7e9383874b45455a6c2973
steve <steve@DESKTOP-FC46IHE>
2021-02-24 15:57:13 -0330

    datasets

---
+++ routes/router.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router  = express.Router()
+
+//const book_controller = require("../controllers/books.js")
+
+router.post("/",)
+router.get("/",)
+router.get("/:id",)
+router.put('/:id',)
+router.delete('/:id',)
+
+module.exports = router
---
+++ test/tests.js
@@ -0,0 +1,60 @@
+var assert = require('assert');
+const request = require('request');
+var myurl = 'http://localhost:3000';
+
+
+describe('Testing the Book API', async function(){
+    describe('Testing- Simple cases', function(){
+
+        it('Fail 1', function(){
+
+        });
+        it('Fail 2', async function(){
+
+        });
+        it('Fail 3', async function(){
+
+        });
+        it('Fail 4', async function(){
+
+        });
+        it('Fail 5', async function(){
+
+        });
+        it('Success 1', async function(){
+
+        });
+        it('Success 2', async function(){
+
+        });
+        it('Success 3', async function(){
+
+        });
+        it('Success 4', async function(){
+
+        });
+        it('Success 5', async function(){
+
+        });
+    });
+    describe('Testing the Book API - Complex Cases', function(){
+        it('Success 1', function(){
+
+        });
+        it('Success 2', function(){
+
+        });
+        it('Success 3', function(){
+
+        });
+        it('Success 4', function(){
+  
+        });
+        it('Success 5', function(){
+
+        });
+        it('Success 6', function(){
+
+        });
+    });
+});
